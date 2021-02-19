4a78b0b54b09ee6db3926342eb6046a4b9045d56
Mohammad sobuj <sobuj10699@diu.edu.bd>
2021-02-20 00:29:22 +0600

    first frontend commit

---
+++ external/externalfile.js
@@ -0,0 +1,3 @@
+function exFunction() {
+    document.getElementById('external').innerHTML=" External JavaScript"
+}
---
+++ myScript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById('demoexternal').innerHTML="First time Using External JavaScript"
+}
